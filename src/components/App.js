9e40fdddcc8bfe8a74ea55f433ca97d781913dc6
trentthom <trent.tt@gmail.com>
2021-05-28 13:10:27 +1000

    head meta tag

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { Component } from "react"
 import Home from './Home'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Movie from './Movie'
+import MetaTags from 'react-meta-tags'
+
 class App extends Component {
   render() {
     return (
       <>
+        <MetaTags>
+          <title>Star Wars Search</title>
+          <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        </MetaTags>
         <Router>
           <Switch>
             <Route path='/' component={Home} exact />
